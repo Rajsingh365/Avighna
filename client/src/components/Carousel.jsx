9a2d1e1414f4f0421ff03a69c664675a730c98b1
Rajsingh365 <sraj08188@gmail.com>
2024-07-16 22:28:51 +0530

    Add text animation

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -4,11 +4,12 @@ function Carousel() {
   const imgList = [
     "https://shorturl.at/U4rkM",
     "https://shorturl.at/NlDls",
-    "https://shorturl.at/2sBKp"
+    "https://shorturl.at/2sBKp",
+    "https://shorturl.at/XyV0b"
   ];
-  
+
   return (
-    <div id="default-carousel" className="relative w-[95%] md:w-[75%]" data-carousel="slide">
+    <div id="animation-carousel" className="relative w-[95%] md:w-[75%]" data-carousel="slide">
       {/* Carousel wrapper */}
       <div className="relative h-56 overflow-hidden rounded-lg md:h-[35rem]">
         <Helper imgList={imgList} />
@@ -18,9 +19,9 @@ function Carousel() {
 }
 
 function Helper({ imgList }) {
-  const divClass = "hidden duration-700 ease-in-out";
+  const divClass = "hidden duration-200 ease-linear";
   const imgClass = "absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2";
-  
+
   return (
     <>
       {imgList.map((img, index) => (
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,10 +2,16 @@ import React, { useEffect } from 'react';
 import GlassMorphism from '../components/GlassMorphism';
 import Carousel from '../components/Carousel';
 import {initFlowbite} from 'flowbite'
+import {useGSAP} from '@gsap/react'
+import gsap from 'gsap'
 function Home() {
   useEffect(()=>{
     initFlowbite()
   },[])
+
+  useGSAP(()=>{
+    gsap.from(".animate-text",{ y: '100%', opacity: 0, delay: 1, duration: 1, ease: 'power1.out', stagger: 0.5 })
+  })
   return (
 
     <div className="py-10 h-min-screen bg-[radial-gradient(circle_at_50%_75%,#FFBA08,#F48C06)] flex flex-col justify-center items-center gap-8">
@@ -13,8 +19,8 @@ function Home() {
         <svg className="w-8 h-8 text-gray-400 dark:text-gray-600 mb-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
           <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
         </svg>
-        “वक्रतुण्ड महाकाय सूर्यकोटि समप्रभ। निर्विघ्नं कुरु मे देव सर्वकार्येषु सर्वदा॥”
-        <p className='text-sm md:text-xl'>O Lord with a curved trunk and immense form, whose radiance is like a million suns, remove all obstacles from my endeavors forever.</p>
+        <p className='text-4xl animate-text'>“वक्रतुण्ड महाकाय सूर्यकोटि समप्रभ। निर्विघ्नं कुरु मे देव सर्वकार्येषु सर्वदा॥”</p>
+        <p className='text-sm md:text-xl animate-text'>O Lord with a curved trunk and immense form, whose radiance is like a million suns, remove all obstacles from my endeavors forever.</p>
       </GlassMorphism>
       <Carousel/>
     </div>
